43a6c1bdb77b64330d0d53b9060c2813ab0d85c4
quangprobee <quangprobee@gmail.com>
2021-06-08 11:18:01 +0700

    comment tai day

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import {  View,Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from "./screens/home/home";
+import VoucherScreen from "./screens/voucher/voucher";
 
 const Stack=createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator  initialRouteName="Voucher"> 
         <Stack.Screen name="Home" component={LoginScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Voucher" component={VoucherScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
